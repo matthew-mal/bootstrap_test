94947233faff349ab1d113e00352d04f60bac083
mat_mal <matvey_maljanov@rambler.ru>
2024-03-10 10:36:25 +0300

    Changed forms fields and added templates

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/home/', permanent=True)),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
