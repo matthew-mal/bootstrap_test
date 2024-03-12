00c474d20ba1833c830a485e07dd57522931a861
mat_mal <matvey_maljanov@rambler.ru>
2024-03-12 13:14:11 +0300

    added tests for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location_homepage_view(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'Home')
