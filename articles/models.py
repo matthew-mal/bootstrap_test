55e6951b4bd7d84a3f52e49e399b1afe86e5c203
mat_mal <matvey_maljanov@rambler.ru>
2024-03-13 13:56:52 +0300

    CRUD

--- articles/models.py
+++ articles/models.py
@@ -13,4 +13,4 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('article-detail', kwargs={'pk': self.pk})
+        return reverse('article_edit', kwargs={'pk': self.pk})
