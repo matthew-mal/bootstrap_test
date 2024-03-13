525d10861109aa453d9b4308eb94f3aef90db1a0
mat_mal <matvey_maljanov@rambler.ru>
2024-03-13 14:02:36 +0300

    improved CreateView

--- articles/views.py
+++ articles/views.py
@@ -35,6 +35,9 @@ class ArticleCreateView(CreateView):
     fields = (
         'title',
         'body',
-        'author',
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
